26cc767578582dd46a03df280d84667740359f69
Mateusz Biszewski <s26716@pjwstk.edu.pl>
2025-03-02 18:25:25 +0100

    Modyfikacja 4

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -2,3 +2,19 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Bye bye, World!");
 // worse
+
+
+int[] arr= {1, 2, 3, 4, 5};
+
+public static double GetAverage(int[] nubers)
+{
+    double result = 0;
+
+    
+    
+    
+    return 0;
+
+}
+
+GetAverage(arr);
